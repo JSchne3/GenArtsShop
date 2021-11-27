b1876bb631f4b719ae405470d4af43ff982748f7
jessica <abyjabbylearns@gmail.com>
2021-11-27 16:05:20 -0600

    Seven

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router() 
-import Product from '../models/productModel.js'
+import {getProductById, getProducts} from '../Controllers/productControllers.js'
 
-// @desc     Fetch all Products
-// @route    GET /api/products
-// @access   public
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
+router.route('/').get(getProducts)
 
-    res.json(products)
-}))
-
-
-// @desc     Fetch single Products
-// @route    GET /api/products/:id
-// @access   public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-})
-)
+router.route('/:id').get(getProductById)
 
 export default router
