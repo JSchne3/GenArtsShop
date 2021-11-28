db7155ce7ecfe8f0561bbf2f58e4163557d53e82
jessica <abyjabbylearns@gmail.com>
2021-11-27 19:54:09 -0600

    8

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router() 
-import {authUser, registerUser, getUserProfile} from '../Controllers/userControllers.js'
+import {authUser, registerUser, getUserProfile, updateUserProfile} from '../Controllers/userControllers.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
