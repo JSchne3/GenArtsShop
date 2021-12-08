4ecc3c84ac661f8f3dcf5dfae5238ff39dbb1a41
jessica <abyjabbylearns@gmail.com>
2021-12-07 23:22:50 -0600

    11

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,30 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux' 
+import {
+    createStore, 
+    combineReducers, 
+    applyMiddleware, 
+} from 'redux' 
 import thunk from 'redux-thunk' 
 import {composeWithDevTools} from 'redux-devtools-extension' 
-import {productListReducer, productDetailsReducer} from './reducers/productReducers'
+import {
+    productListReducer, 
+    productDetailsReducer, 
+} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
-import {orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer} from './reducers/orderReducers'
+import {
+    userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer, 
+    userUpdateProfileReducer, 
+    userListReducer, 
+    userDeleteReducer, 
+    userUpdateReducer, 
+} from './reducers/userReducers'
+import {
+    orderCreateReducer, 
+    orderDetailsReducer, 
+    orderPayReducer, 
+    orderListMyReducer, 
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer, 
@@ -14,6 +34,9 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer, 
     userDetails: userDetailsReducer, 
     userUpdateProfile: userUpdateProfileReducer, 
+    userList: userListReducer, 
+    userDelete: userDeleteReducer, 
+    userUpdate: userUpdateReducer, 
     orderCreate: orderCreateReducer, 
     orderDetails: orderDetailsReducer, 
     orderPay: orderPayReducer, 
