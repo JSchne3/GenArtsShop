118dc67ca59a1d6a8fbac541450b33c0449f896d
jessica <abyjabbylearns@gmail.com>
2022-02-17 10:25:58 -0600

    initial

---
+++ frontend/build/precache-manifest.1e5b184615d38c99656f2cad02111e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f46168d9b17735c4a9cfeed43ae081b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2556d9ff6b26223f51",
+    "url": "/static/css/main.b823836f.chunk.css"
+  },
+  {
+    "revision": "cbf0a4da2b916139d0d2",
+    "url": "/static/js/2.20972ccc.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.20972ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ed9ab1eb2ee56268de",
+    "url": "/static/js/3.5ac9618b.chunk.js"
+  },
+  {
+    "revision": "2c2556d9ff6b26223f51",
+    "url": "/static/js/main.0b1e810b.chunk.js"
+  },
+  {
+    "revision": "e3931be6ae3cd07de985",
+    "url": "/static/js/runtime-main.1c6a9f49.js"
+  }
+]);
